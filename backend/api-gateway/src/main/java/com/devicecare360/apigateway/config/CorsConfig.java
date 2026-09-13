d0d9ae9087fc5f86326b7a05055142584fb1fb8a
AHARazzak <hameedabdulrazzak@gmail.com>
2026-09-13 19:08:45 +0530

    fix: update cors allowed headers to wildcard

--- backend/api-gateway/src/main/java/com/devicecare360/apigateway/config/CorsConfig.java
+++ backend/api-gateway/src/main/java/com/devicecare360/apigateway/config/CorsConfig.java
@@ -17,7 +17,7 @@ import reactor.core.publisher.Mono;
 @Configuration
 public class CorsConfig {
 
-    private static final String ALLOWED_HEADERS = "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN, X-User-Id, X-User-Role, X-User-Name";
+    private static final String ALLOWED_HEADERS = "*";
     private static final String ALLOWED_METHODS = "GET, PUT, POST, DELETE, OPTIONS, PATCH";
     private static final String MAX_AGE = "3600";
 
